1c9a46b1a38d9bafc5df8d94f01a0ab55f6450a4
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-01-18 11:06:33 -0700

    Changed sql directory name back to its original name.

---
+++ sql/oml.sql
@@ -0,0 +1,15 @@
+INSERT INTO user (userId, userActivationToken, userName, userEmail, userHash) VALUES (
+    UNHEX("UUID()"), 
+    "nananananananananananananananana", 
+    "jTredway", 
+    "jwilliamtredway@gmail.com",
+    "nananananananananananananananananananananananananananananananananananananananananananananananana"
+    );
+    
+    
+    
+    
+    
+    
+    
+    
